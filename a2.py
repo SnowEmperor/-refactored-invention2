0122fec972c566868d9d2715bd65167ca657c986
SnowEmperor <99719069+SnowEmperor@users.noreply.github.com>
2022-02-15 12:28:09 +0300

    Delete a2.py

--- a2.py
+++
@@ -1,9 +0,0 @@
-import argparse
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument('scan_file', type=str)
-    parser.add_argument('name_file', type=str)
-    args = parser.parse_args()
-    q = {}
-from a1 import path_dump
-path_dump(args.scan_file, args.name_file)
