a3d36f86caa04da674f6aba55f887b716bef3ca3
SnowEmperor <99719069+SnowEmperor@users.noreply.github.com>
2022-03-11 11:26:23 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,101 @@
-from fastapi import FastAPI
+import sqlite3
+from a1 import path_dump
+from typing import Optional
+from fastapi import Depends, FastAPI, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from pydantic import BaseModel
 
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "hashed_password": "secret",
+        "disabled": False,
+    }
+}
+
+
+def tabler(records):
+    conn = sqlite3.connect('orders.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+        name TEXT,
+        path TEXT);
+         """)
+    cur.executemany("INSERT INTO users VALUES(?, ?);", records)
+    conn.commit()
+    conn.close()
+
+
+def fake_hash_password(password: str):
+    return password
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+class User(BaseModel):
+    username: str
+    disabled: Optional[bool] = None
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return UserInDB(**user_dict)
+
+
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+
+async def get_current_active_user(current_user: User = Depends(get_current_user)):
+    if current_user.disabled:
+        raise HTTPException(status_code=400, detail="Inactive user")
+    return current_user
 
 app = FastAPI()
 
 
-@app.post("/items/{item_id}")
-def dump_file(path_scan: str, file_name: str):
-    from a322 import path_dump
-    result = path_dump(path_scan, file_name)
-    return result
+@app.post("/token")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(current_user: User = Depends(get_current_active_user)):
+    return current_user
+
+
+@app.post("/")
+async def create_note(scan_file, name_file, current_user: User = Depends(get_current_active_user)):
+    result = path_dump(scan_file, name_file)
+    file_path = list(result.items())
+    tabler(file_path)
+    return
+
+
+
